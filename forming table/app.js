4d647fc1c36dc8d3d3a26dce6ccbf33614947241
Hafsa Naveed <hafsanaveed207@gmail.com>
2024-07-20 14:42:19 +0500

    updated app.js

--- forming table/app.js
+++ forming table/app.js
@@ -1,5 +1,5 @@
 var table = prompt("write a number and i will print its table");
 
 for(let i = 1; i <= 10; i++){
-alert(table + " x " + i + " = " + i*table);
+console.log(table + " x " + i + " = " + i*table);
 }
